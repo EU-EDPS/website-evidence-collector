269f74f08749cb81d87b602b2222f6ed8305d39b
Robert Riemann <robert.riemann@edps.europa.eu>
2019-04-17 14:41:52 +0200

    0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## HEAD
 
+## 0.1.0 / 2019-04-17
+
   * Birthday!
   * move monkey-patched code to a proper repository
   * create README.md and CHANGELOG.md
+  * feature: use `Debugger.scriptParsed` and heuristical assumptions to get insights on when cookies are placed
--- website-evidence-collector.js
+++ website-evidence-collector.js
@@ -1,19 +1,77 @@
-// change global directory with npm config edit
-// install puppeteer globally with npm save -g puppeteer
-// link puppeteer locally with npm link puppeteer
-
 const puppeteer = require('puppeteer');
 
+const detailedDiff = require("deep-object-diff").detailedDiff;
+
+const _ = require('lodash');
+
+// const cookieParse = require('cookie').parse;
 
 (async() => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: '/home/rriemann/lib/node_modules/puppeteer/.local-chromium/linux-609904/chrome-linux/chrome',
+  });
   const page = await browser.newPage();
-  const url = process.argv[2];
-  await page.goto(url, {waitUntil : 'networkidle2' });
 
-  // example from https://stackoverflow.com/a/50290081/1407622
-  // Here we can get all of the cookies
-  console.log(await page._client.send('Network.getAllCookies'));
+  var cookies = {};
+  var cookiesByURL = {};
+
+  // https://github.com/jasonLaster/cdp-pause-points/blob/9260aafda97ebe975fc6d9a03979c8866ebd4b0e/pause-points.js#L66
+  const client = await page.target().createCDPSession();
+  await client.send('Debugger.enable');
+
+  // https://github.com/GoogleChrome/puppeteer/issues/313#issuecomment-322939468
+  page.on('response', async(response) => {
+    const req = response.request();
+    console.log(req.method(), response.status(), req.url());
+    // console.log(response._headers['Set-Cookie']);
+    // console.log(response._headers);
+    let httpCookie = response._headers['set-cookie'];
+    console.log("COOKIE: " + httpCookie);
+    if(httpCookie) {
+      // cookieName = httpCookie.match(/^[^=]+/)[0];
+      // Object.keys(cookieParse(httpCookie))
+      httpCookie.split("\n").forEach( cookieValue => {
+        cookieName = cookieValue.match(/^[^=]+/)[0]
+        if(Array.isArray(cookiesByURL[cookieName])) {
+          cookiesByURL[cookieName].push(req.url());
+        } else {
+          cookiesByURL[cookieName] = [req.url()];
+        }
+      });
+    }
+  });
+
+  // track cookies
+  var processCookies = async function(script) {
+    console.log("Script: " + script.url);
+    // example from https://stackoverflow.com/a/50290081/1407622
+    let currentCookies = _.cloneDeep(cookies);
+    let newCookies = await client.send('Network.getAllCookies');
+
+    let diff = detailedDiff(currentCookies, newCookies['cookies']);
+
+    // focus on added and updated
+    (Object.values(diff['added']).concat(Object.values(diff['updated']))).forEach( cookie => {
+      if(Array.isArray(cookiesByURL[cookie['name']])) {
+        cookiesByURL[cookie['name']].push(script.url);
+      } else {
+        cookiesByURL[cookie['name']] = [script.url];
+      }
+    });
+
+    cookies = newCookies['cookies'];
+  }
+
+  client.on("Debugger.scriptParsed", processCookies)
+
+  const url = process.argv[2];
+  await page.goto(url, {waitUntil : 'networkidle2', timeout: 0 });
+
+  cookies = await page._client.send('Network.getAllCookies');
+
+  console.log(cookiesByURL)
+
+  console.log(cookies);
 
   // await page.screenshot({path: 'example.png'});
 
