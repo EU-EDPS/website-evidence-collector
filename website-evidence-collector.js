b75ba18ff861fb0f99a003945d3fb367c83641d3
Robert Riemann <robert.riemann@edps.europa.eu>
2019-05-16 15:48:49 +0200

    add easyprivacy matching

--- website-evidence-collector.js
+++ website-evidence-collector.js
@@ -9,6 +9,29 @@ const PuppeteerHar = require('puppeteer-har');
 const fs = require('fs');
 const cookieParser = require('tough-cookie').Cookie;
 
+const { FiltersEngine, NetworkFilter, makeRequest } = require('@cliqz/adblocker');
+const { parse } = require('tldts');
+
+// setup easyprivacy matching
+// https://github.com/cliqz-oss/adblocker/issues/123
+let filtersEngine = FiltersEngine.parse(fs.readFileSync('assets/easyprivacy.txt', 'utf8'), {loadCosmeticFilters: false});
+const typesMapping = {
+  document: 'main_frame',
+  eventsource: 'other',
+  fetch: 'xhr',
+  font: 'font',
+  image: 'image',
+  manifest: 'other',
+  media: 'media',
+  other: 'other',
+  script: 'script',
+  stylesheet: 'stylesheet',
+  texttrack: 'other',
+  websocket: 'websocket',
+  xhr: 'xhr',
+};
+
+
 (async() => {
   const browser = await puppeteer.launch({
     // headless: false,
@@ -127,6 +150,35 @@ const cookieParser = require('tough-cookie').Cookie;
   });
 
   const url = process.argv[2];
+
+  // prepare easyprivacy list matching
+  // await page.setRequestInterception(true);
+  page.on('request', (request) => {
+    let ts = new Date();
+    const {
+      match, // `true` if there is a match
+      redirect, // data url to redirect to if any
+      exception, // instance of NetworkFilter exception if any
+      filter, // instance of NetworkFilter which matched
+    } = filtersEngine.match(makeRequest({
+      sourceUrl: request.frame().url(),
+      type: typesMapping[request.resourceType()],
+      url: request.url(),
+    }, parse));
+    if(match) {
+      // console.log("easyprivacy: " + request.url());
+      let stack = [{
+        filenName: request.frame().url(),
+        source: `requested from ${request.frame().url()} and matched with easyprivacy.txt filter ${filter}`,
+      }];
+      reportedEvents.push({
+        type: "Request.Tracking",
+        stack: stack,
+        ts: ts,
+        data: request.url(),
+      });
+    }
+  });
 
   // recording har file if 2nd commandline argument is set
   const harFile = process.argv[3]; // is undefined if not set
