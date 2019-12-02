ddca75c820841c35c2fa06aadd6a5cfce98752af
Robert Riemann <robert.riemann@edps.europa.eu>
2019-12-02 14:05:08 +0100

    improve reporting tool

--- website-evidence-reporter.js
+++ website-evidence-reporter.js
@@ -21,17 +21,22 @@ var argv = require('yargs') // TODO use rather option('o', hash) syntax and defi
   // top-level default command, see https://github.com/yargs/yargs/blob/master/docs/advanced.md#default-commands
   .demandCommand(1, 'An input JSON file is mandatory.') // ask for command and for inspection url
 
-  .describe('html-template', 'Custom pug template to generate HTML')
+  .describe('html-template', 'Custom pug template to generate HTML output')
   .nargs('html-template', 1)
+  .alias('html-template', 't')
+  .string('html-template')
+
+
+  .describe('output', 'Write HTML output to file instead to the screen')
+  .nargs('output', 1)
+  .alias('output', 'o')
+  .string('output')
 
   .help('help')
   .alias('h', 'help')
   .epilog('Copyright European Union 2019, licensed under EUPL-1.2 (see LICENSE.txt)')
   .argv;
-
-// use in shell for development:
-// while inotifywait -e modify assets/template.pug; do ./create-html.js output_bild/inspection.json > output_bild/inspection.html; done
-
+  
 let output = JSON.parse(fs.readFileSync(argv._[0]));
 
 let html_template = argv.html_template || path.join(__dirname, 'assets/template.pug');
@@ -42,4 +47,8 @@ let html_dump = pug.renderFile(html_template, Object.assign({}, output, {
   groupBy: require('lodash/groupBy'),
 }));
 
-console.log(html_dump);
+if (argv.output) {
+  fs.writeFileSync(path.join(argv.output), html_dump);
+} else {
+  console.log(html_dump);
+}
