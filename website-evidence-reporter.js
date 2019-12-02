05214d452d63b9dc756aa1e956079abb3ff397d1
Robert Riemann <robert.riemann@edps.europa.eu>
2019-12-02 12:25:18 +0100

    feature: add new tool website-evidence-reporter to allow generation of HTML output from JSON output without repeating the evidence collection.
      Together with custom templates, this allows to generate different HTML 
    reports from the same data. It also helps the development/debugging of 
    custom templates.

---
+++ website-evidence-reporter.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+// jshint esversion: 8
+
+/**
+ * @file Collects evidence from websites on processed data in transit and at rest.
+ * @author Robert Riemann <robert.riemann@edps.europa.eu>
+ * @copyright European Data Protection Supervisor (2019)
+ * @license EUPL-1.2
+ *
+ * @example while inotifywait -e modify assets/template.pug; do ./create-html.js output/inspection.json > output/inspection.html; done
+ */
+
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+
+var argv = require('yargs') // TODO use rather option('o', hash) syntax and define default top-level command
+  .scriptName('website-evidence-reporter')
+  .usage('Usage: $0 <JSON file> [options]')
+  .example('$0 /home/user/inspection.json')
+  // top-level default command, see https://github.com/yargs/yargs/blob/master/docs/advanced.md#default-commands
+  .demandCommand(1, 'An input JSON file is mandatory.') // ask for command and for inspection url
+
+  .describe('html-template', 'Custom pug template to generate HTML')
+  .nargs('html-template', 1)
+
+  .help('help')
+  .alias('h', 'help')
+  .epilog('Copyright European Union 2019, licensed under EUPL-1.2 (see LICENSE.txt)')
+  .argv;
+
+// use in shell for development:
+// while inotifywait -e modify assets/template.pug; do ./create-html.js output_bild/inspection.json > output_bild/inspection.html; done
+
+let output = JSON.parse(fs.readFileSync(argv._[0]));
+
+let html_template = argv.html_template || path.join(__dirname, 'assets/template.pug');
+
+let html_dump = pug.renderFile(html_template, Object.assign({}, output, {
+  pretty: true,
+  basedir: __dirname,
+  groupBy: require('lodash/groupBy'),
+}));
+
+console.log(html_dump);
