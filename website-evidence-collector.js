642fa013220269f25f6bf0b9f8fc3c2975eba170
BitnessWise <marty.korse+github@gmail.com>
2020-02-11 09:29:14 +0100

    added support for cookies, similar to curl -b

--- website-evidence-collector.js
+++ website-evidence-collector.js
@@ -38,6 +38,10 @@ const { isFirstParty, getLocalStorage } = require('./lib/tools');
 const uri_ins = argv._[0];
 const uri_ins_host = url.parse(uri_ins).hostname; // hostname does not include port unlike host
 
+// default the cookie expiration time, set to 5 minutes into the future.
+// This is a workaround because it seems session cookies (with expiration 0) are not sent
+const cookieDefaultExpirationTime = Math.floor(Date.now() / 1000) + 300
+
 var uri_refs = [uri_ins].concat(argv.firstPartyUri);
 
 let uri_refs_stripped = uri_refs.map((uri_ref) => {
@@ -185,6 +189,89 @@ var refs_regexp = new RegExp(`^(${uri_refs_stripped.join('|')})\\b`, 'i');
   await har.start({ path: argv.output ? path.join(argv.output, 'requests.har') : undefined });
 
   logger.log('info', `browsing now to ${uri_ins}`, {type: 'Browser'});
+
+  // check if cookies need to be added
+  if (argv.cookie) {
+    // variable that will buffer all the valid cookies that are passed
+    let cookieJar = [];
+    if (fs.existsSync(argv.cookie)) {
+      // passed argument is the location of a file
+      logger.log('info', 'cookie parameter is an existing file; reading values from '+argv.cookie);
+      let requestedDomain, protocol;
+      if (uri_ins.indexOf('http') == 0) {
+        requestedDomain = uri_ins.split('/')[2];
+        protocol = uri_ins.split(':')[0];
+      }
+      else {
+        requestedDomain = uri_ins.split('/')[0];
+        protocol = "http";
+      }
+      // cookiefile should be small, so reading it into memory
+      const lines = fs.readFileSync(argv.cookie, 'UTF-8').trim().split(/\r?\n/);
+      for(let line of lines) {
+        if (line.trim().indexOf('#') == 0) {
+          // this line is a comment; not parsing it
+          continue;
+        }
+        let cookieArr = line.split('\t');
+        if (cookieArr.length == 7) {
+          if (cookieArr[0] != requestedDomain || (protocol == "http" && cookieArr[3])) {
+            logger.info('info', line+' doesn\'t match the requested domain or http url when cookie is https-only');
+            continue;
+          }
+          // valid line, adding cookie
+          /***
+           * Netscape cookie file format, equal to how curl uses it: 
+           *
+           * 0 | string | example.com | Domain name
+           * 1 | boolean | FALSE | Include subdomains
+           * 2 | string | /foobar/ | Path
+           * 3 | boolean | TRUE | Send/receive over HTTPS only
+           * 4 | number | 1462299217 | Expires at – seconds since Jan 1st 1970, or 0
+           * 5 | string | person | Name of the cookie
+           * 6 | string | daniel | Value of the cookie
+           */
+          // add to the buffer
+          cookieJar.push({
+            value: cookieArr[6],
+            expires: cookieArr[4] == "0" ? cookieDefaultExpirationTime : cookieArr[4],
+            url: protocol+'://'+cookieArr[0]+cookieArr[2],
+            name: cookieArr[5]
+          });
+        }
+        else {
+          logger.log('error', 'invalid formatted line - skipping it : '+line);
+        }
+      }
+    }
+    else {
+      logger.log('info', 'cookie parameter is not an existing file; parsing it as key=value pairs');
+      let jarArr = argv.cookie.split(";");
+      for (let cookieStr of jarArr) {
+        if (cookieStr.indexOf("=") >= 0) {
+          let cookieName = cookieStr.split(/=(.+)/)[0], cookieValue = cookieStr.split(/=(.+)/)[1];
+          cookieJar.push({
+            value: cookieValue,
+            expires: cookieDefaultExpirationTime,
+            url: uri_ins,
+            name: cookieName
+          });
+        }
+      }
+    }
+    // adding cookies from the buffer if we have them
+    for (var c in cookieJar) {
+      var cookie = cookieJar[c];
+      logger.log('info', 'setting initial cookie : '+cookie.name+'='+cookie.value+' for request '+cookie.url+' with expiration '+cookie.expires);
+      await page.setCookie({
+        "value": cookie.value,
+        "expires": cookie.expires,
+        "url": cookie.url,
+        "name": cookie.name
+      });
+    }
+  }
+
   let page_response;
   try {
     page_response = await page.goto(uri_ins, {timeout: argv.pageTimeout, waitUntil : 'networkidle2' });
